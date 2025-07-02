b5e0e6359154ce3530746f00828acda75da7bb51
Svenez96 <mazzadomenico96@yahoo.com>
2025-07-02 08:46:20 +0200

    exercise complete

--- week-3/src/hooks/useFilteredTodos.js
+++ week-3/src/hooks/useFilteredTodos.js
@@ -1,7 +1,18 @@
-export const useFilteredTodos = (todos, searchTerm) => {
-    return todos.filter(todo => 
-        todo.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-};
+import { useState,  useEffect } from 'react';
 
+const useFilteredTodos = (todos, searchTerm) => {
+    const [filteredTodos, setFilteredTodos] = useState([]);
+
+    useEffect(() => {
+        const term = searchTerm.toLowerCase();
+        
+        const result = todos.filter(todo => 
+            todo.title.toLowerCase().includes(term)
+        );
+
+        setFilteredTodos(result);
+    }, [todos, searchTerm]);
+
+    return filteredTodos;
+}
 export default useFilteredTodos;
