f919268b4833651244ffa78e86de7a460d8a7f99
Svenez96 <mazzadomenico96@yahoo.com>
2025-07-02 09:53:09 +0200

    useRef on input

--- week-3/src/components/TodoList.jsx
+++ week-3/src/components/TodoList.jsx
@@ -1,27 +1,39 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useMemo, useRef, useEffect} from "react";
 import { useFetch } from "../hooks/useFetch";
-import useFilteredTodos from "../hooks/useFilteredTodos";
 
 const API_URL = 'https://jsonplaceholder.typicode.com/todos';
 
-const TodoList = ({todos}) => {
+const TodoList = () => {
     const { data, loading, error } = useFetch(API_URL);
     const [searchTerm, setSearchTerm] = useState("");
-
+    const inputRef = useRef(null);
+    
     const handleSearchChange = useCallback((e) => {
         console.log("handleSearchChange triggered");
         setSearchTerm(e.target.value);
     }, []);
-
-    const filteredTodos = useFilteredTodos(data || [], searchTerm);
+    
+    const filteredTodos = useMemo(() => {
+        if (!data) return [];
+        return data.filter(todo =>
+            todo.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    }, [data, searchTerm]);
+    
+    useEffect(() => {
+        if(inputRef.current){
+            inputRef.current.focus();
+        }
+    }, []);
     
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
     if (!data) return null;
+    
 
     return (
         <>
-            <input type="text" value={searchTerm} onChange={handleSearchChange}
+            <input type="text" ref={inputRef} value={searchTerm} onChange={handleSearchChange}
                 placeholder="Search" />
             <ul>
                 {filteredTodos.map((item) => (
